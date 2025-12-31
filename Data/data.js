dc319a531326ab0eeb1a188dd8c6a0acbd863d45
Aalekh-coder <dheetaalekh@gmail.com>
2025-12-31 15:43:24 +0530

    image change and navbar

--- Data/data.js
+++ Data/data.js
@@ -1,4 +1,3 @@
-
 // data/portfolio.js
 export const portfolioItems = [
   {
@@ -20,127 +19,126 @@ export const portfolioItems = [
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 4,
     image: "/portfolio/test4.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test5.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
-   
 
-    {
+  {
     id: 6,
     image: "/portfolio/test6.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test7.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test8.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test9.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test10.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test11.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test12.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test13.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-    {
+  {
     id: 5,
     image: "/portfolio/test14.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-     {
+  {
     id: 5,
     image: "/portfolio/test15.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-     {
+  {
     id: 5,
     image: "/portfolio/test16.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-     {
+  {
     id: 5,
     image: "/portfolio/test17.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-     {
+  {
     id: 5,
     image: "/portfolio/test18.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-     {
+  {
     id: 5,
     image: "/portfolio/test19.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-     {
+  {
     id: 5,
     image: "/portfolio/test20.jpg",
     title: "Pharma Exhibition Stall",
     location: "Mumbai Expo",
   },
 
-     {
+  {
     id: 5,
     image: "/portfolio/test21.jpg",
     title: "Pharma Exhibition Stall",
@@ -148,74 +146,44 @@ export const portfolioItems = [
   },
 ];
 
-
-
-
 // /data/categories.js
 
-
-
-
-
 export const categories = [
-
-
-
-
-  {
-    id: "corporate-event-management",
-    name: "Corporate Event Management",
-   desc:"We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
-
-  },
-
-   {
-    id: "brand-activation",
-    name: "Brand Activation",
-   desc:"We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
-
-  },
-   {
-    id: "product-launch",
-    name: "Product Launch",
-   desc:"We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
-   image:"/service4.jpg",
-  },
-   {
-    id: "graphic-design",
-    name: "Graphic Design ",
-   desc:"We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
-   image:"/service4.jpg",
-  },
- 
- 
   {
     id: "domestic-exhibition-stall-designing-services",
     name: "Domestic Exhibition Stall Designing Services",
-    desc:"Service Provider of a wide range of services which include Exhibition Stall Designing Services, Stall Designing Services, Exhibition Designing Services, Exhibition Pavilion Designing Services and ITB,Berlin 2013 Exhibition Stall Designing.",
-
-
-   
+    desc: "Service Provider of a wide range of services which include Exhibition Stall Designing Services, Stall Designing Services, Exhibition Designing Services, Exhibition Pavilion Designing Services and ITB,Berlin 2013 Exhibition Stall Designing.",
   },
-
-
-   {
+  {
     id: "international-exhibition-stall-designing-services",
     name: "international Exhibition Stall Designing Services",
-    desc:"Service Provider of a wide range of services which include Exhibition Stall Designing Services, Stall Designing Services, Exhibition Designing Services, Exhibition Pavilion Designing Services and ITB,Berlin 2013 Exhibition Stall Designing.",
-   
-
-   
+    desc: "Service Provider of a wide range of services which include Exhibition Stall Designing Services, Stall Designing Services, Exhibition Designing Services, Exhibition Pavilion Designing Services and ITB,Berlin 2013 Exhibition Stall Designing.",
   },
-
-
-
   {
     id: "exhibition-fabrication-services",
     name: "Exhibition Fabrication Services",
-     desc: "We are counted amongst the prominent service providers engaged in offering exhibition design fabrication services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area.",
-  
-    
+    desc: "We are counted amongst the prominent service providers engaged in offering exhibition design fabrication services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area.",
   },
-
+  {
+    id: "corporate-event-management",
+    name: "Corporate Event Management",
+    desc: "We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
+  },
+  {
+    id: "brand-activation",
+    name: "Brand Activation",
+    desc: "We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
+  },
+  {
+    id: "product-launch",
+    name: "Product Launch",
+    desc: "We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
+    image: "/service4.jpg",
+  },
+  {
+    id: "graphic-design",
+    name: "Graphic Design ",
+    desc: "We are counted amongst the prominent service providers engaged in offering Corporate Event Management Services to the valuable clients. We are supported by dedicated designers who make optimum utilization of the allotted area, in order to plan and execute these services.",
+    image: "/service4.jpg",
+  },
 ];
