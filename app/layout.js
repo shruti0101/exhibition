653fb8479120c1250bbd74e3a60828a358166d23
Anshumantyagi2004 <161432160+Anshumantyagi2004@users.noreply.github.com>
2026-01-31 12:00:27 +0530

    clarity-script add on

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "v9x966pvj5");
+    `,
+          }}
+        />
+        <Script
           id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
