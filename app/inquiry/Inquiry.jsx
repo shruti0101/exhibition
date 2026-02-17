b034127acacef7cae6055f022cfb43a6b979b991
Anshumantyagi2004 <161432160+Anshumantyagi2004@users.noreply.github.com>
2026-02-17 15:06:26 +0530

    inquiry hero section  chnages

--- app/inquiry/Inquiry.jsx
+++ app/inquiry/Inquiry.jsx
@@ -216,27 +216,29 @@ export default function InquiryPage() {
           <div className="relative container mx-auto px-2 lg:py-15 py-4">
             <div className="grid grid-cols-1 items-center md:gap-12 gap-5 lg:grid-cols-2">
               <div className='lg:pl-8 flex flex-col justify-center lg:justify-start'>
-                <h1 className="mb-4 lg:text-4xl text-lg font-extrabold text-white">
-                  Be the Showstopper at Every Event with Premium Exhibition Stall Designs.
+                <h1 className="mb-4 lg:text-4xl text-xl font-extrabold text-white">
+                  Make Your Brand Stand Out at Every Event & Exhibition
                 </h1>
 
                 <p className="mb-2 max-w-xl text-white md:block hidden lg:text-xl font-semibold">
-                  Strides Design is a leading Exhibition Stall Designer
-                  delivering world-class exhibition stands that attract
-                  attention, engage visitors, and elevate your brand presence.
+                  Strides Design Studio specializes in Exhibition Stalls, Corporate Events, Event Management, and Creative Design across Delhi NCR & Pan India. We don’t just build stalls—we create experiences that attract, engage & convert.
                 </p>
 
-                <p className="md:mb-8 mb-4 max-w-xl text-white font-semibold lg:text-xl text-base">
-                  We specialize in custom exhibition stall design, exhibition stand fabrication, and Exhibition Fabrication Services to ensure your brand space stands out at every trade show and premier industry event.
-                </p>
+                <ul className="list-disc list-inside text-white space-y-2 max-w-xl font-semibold lg:text-xl text-base">
+                  <li>Custom Exhibition Stall Designs</li>
+                  <li>3D Visualization Before Fabrication</li>
+                  <li>Complete Design to Execution Solutions</li>
+                  <li>Pan-India Fabrication & Installation Support</li>
+                </ul>
 
-                <div className="flex flex-wrap gap-4 justify-center lg:justify-start">
+
+                <div className="flex flex-wrap gap-4 justify-center lg:justify-start mt-4">
                   <a href='tel:+919953686374' className="group relative inline-flex cursor-pointer items-center gap-2 rounded-full border-2 border-red-600 hover:border-red-700 bg-red-600 px-8 py-2.5 text-sm font-semibold uppercase tracking-wide text-white transition-all duration-300 hover:bg-red-700">
                     Call Us
                     <span className="block h-[1px] w-0 bg-white transition-all duration-300 group-hover:w-6" />
                   </a>
 
-                  <a href="https://wa.me/+919999402424" className="group relative inline-flex cursor-pointer items-center gap-2 rounded-full border-2 border-green-600 hover:border-green-700 bg-green-600 px-8 py-2.5 text-sm font-semibold uppercase tracking-wide text-white transition-all duration-300 hover:bg-green-700">
+                  <a href="https://wa.me/+919999402424" className="group relative inline-flex cursor-pointer items-center gap-2 rounded-full border-2 bg-[#039C98] border-[#039C98] px-8 py-2.5 text-sm font-semibold uppercase tracking-wide text-white transition-all duration-300">
                     Whatsapp
                     <span className="block h-[1px] w-0 bg-white transition-all duration-300 group-hover:w-6" />
                   </a>
